b9710570920a0e219fa4ec3e71e8865241eba1b3
Ahmed <ahmed.alhassan97@hotmail.com>
2023-01-18 12:49:47 +0200

    Button visible when input is not empty

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UsernameCheckComponent } from './username-check/username-check.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsernameCheckComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
